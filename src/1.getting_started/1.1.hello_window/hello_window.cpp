d721bf2c8965451e3ae0bc8035f52044b5f564f0
Hash <HashWaney@gmail.com>
2020-06-28 15:55:22 +0800

    window的一些基本流程与操作

--- src/1.getting_started/1.1.hello_window/hello_window.cpp
+++ src/1.getting_started/1.1.hello_window/hello_window.cpp
@@ -6,6 +6,12 @@
 #include "glfw3.h"
 #include "iostream"
 
+// 将glad.h文件放在所有的头文件之前，不然会报如下错误。
+//#include <glad/glad.h>必须放在所有头文件之前，不管在哪里，不管在.h还是.cpp;
+//error: C1189: #error: OpenGL header already included, remove this include, glad already provides it.
+void processInput(GLFWwindow *window);
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
 using namespace std;
 
@@ -28,27 +34,62 @@ int main() {
     }
     //创建完窗口就可以通知GLFW将窗口的上下文设置为当前线程的主上下文
     glfwMakeContextCurrent(window);
+    //根据缓冲区的大小来调整所谓的窗口大小
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // GLAD
-    // 定义函数原型:如果调用OpenGL的函数就需要先定义一个函数的原型，
-    // 然后找到正确的函数并且赋值给函数指针，这个时候才能去调用函数；不能直接去调用OpenGL的函数
-    // 而将找到正确匹配的OpenGL函数，赋值给我们定义的函数指针，通过函数指针来调用OpenGl的函数。
 
-    typedef void(*GL_GENBUFFERS)(GLsizei, GLuint *);
-    //找到正确的函数并赋值给函数指针
-    GL_GENBUFFERS glGenbuffers = (GL_GENBUFFERS) glfwGetProcAddress("glGenBuffers");
-    //现在函数可以被正常调用了
-    GLuint buffer;
+//
+//    typedef void(*GL_GENBUFFERS)(GLsizei, GLuint *);
+//    //找到正确的函数并赋值给函数指针
+//    GL_GENBUFFERS glGenbuffers = (GL_GENBUFFERS) glfwGetProcAddress("glGenBuffers");
+//    //现在函数可以被正常调用了
+//    GLuint buffer;
 
 //    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     // glad: load all OpenGL function pointers
     // ---------------------------------------
-
-    typedef void* (* GLADloadproc)(const char *name);
+// GLAD
+    // 定义函数原型:如果调用OpenGL的函数就需要先定义一个函数的原型，
+    // 然后找到正确的函数并且赋值给函数指针，这个时候才能去调用函数；不能直接去调用OpenGL的函数
+    // 而将找到正确匹配的OpenGL函数，赋值给我们定义的函数指针，通过函数指针来调用OpenGl的函数。
+    //GLAD传入了用来加载系统相关的OpenGL函数指针地址的函数
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-
+        cout << "Failed to initialize GLAD " << endl;
+        return -1;
     }
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    // 渲染循环
+    while (!glfwWindowShouldClose(window)) {
+        // 处理所有输入：查询GLFW是否在此帧中按下/释放了相关的按键并做出相应的反应
+        processInput(window);
 
-
+        //交换缓冲区数据并将颜色显示到屏幕上,并且轮训IO事件（按键点击或被释放，鼠标移动等等）
+        glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+        cout << "clear color\n " << endl;
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        cout << "poll event\n" << endl; //
+        glfwPollEvents(); // 我一直调用你 ，就是为了去获取你的一些与系统窗口交互的事件来阻断我的判断条件
+        //说白了就是去  glfwSetWindowShouldClose(window, GL_TRUE); 拿这个函数，
+        // 也就是说glfwPollEvents 这个函数一定是与processInput这个函数中的glfwGetKey()是对应的
+    }
+    //清空GLFW分配的资源
+    glfwTerminate();
     return 0;
+}
+
+
+void processInput(GLFWwindow *window) {
+    // 左上角的ESC按键
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        // 如果被按下，设置程序窗口的关闭状态为true
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+// 当操作或者说用户操作导致的窗口大戏变化，这个函数就会被回调
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+
 }
